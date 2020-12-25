c89090771b5b12200bb9c851b5862a0140b282c2
Ivan Lapunka <ivan_lapunka@gmail.com>
2020-12-25 16:58:35 +0300

    Add logging

--- 1_maven/Plain2/main/src/main/java/by/academy/app/Home.java
+++ 1_maven/Plain2/main/src/main/java/by/academy/app/Home.java
@@ -1,5 +1,8 @@
 package by.academy.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -10,16 +13,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class Home extends HttpServlet {
-    @Override
-    public void init() throws ServletException {
-
-    }
-
+    Logger log = LoggerFactory.getLogger(Home.class);
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //super.service(req, resp);
         String login = req.getParameter("param_login");
         String password = req.getParameter("param_password");
+        log.info("Login {}", login);
+        log.error("Runtime Ezxception", new RuntimeException("test"));
         RequestDispatcher requestDispatcher = req.getRequestDispatcher("/redirect");
 
         requestDispatcher.forward(req, resp);
